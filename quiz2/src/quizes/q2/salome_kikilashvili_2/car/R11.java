de8b5fb5ba50aa11b76a37ed2eccf153d295e6dc
Saallome <162875890+Saallome@users.noreply.github.com>
2024-03-25 12:07:33 +0400

    Add files via upload

---
+++ quiz2/src/quizes/q2/salome_kikilashvili_2/car/R11.java
@@ -0,0 +1,9 @@
+package quizes.q2.salome_kikilashvili_2.car;
+import quizes.q2.salome_kikilashvili_2.marker.R12;
+
+public class R11 {
+    public static void main(String[] args) {
+        R12 r12 = new R12();
+        r12.printMessage();
+    }
+}
---
+++ quiz2/src/quizes/q2/salome_kikilashvili_2/marker/R12.java
@@ -0,0 +1,8 @@
+package quizes.q2.salome_kikilashvili_2.marker;
+
+public class R12 {
+    public void printMessage() {
+        System.out.println("Hello from the R12");
+    }
+
+}
